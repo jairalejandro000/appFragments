ad0f387384e6002dbd2a0b521a0a9342cd26906d
jair alejandro <jairalejandro32@outlook.com>
2021-11-20 20:09:54 -0600

    toolbar

--- app/src/main/java/com/example/myapplicationmd/MainActivity.java
+++ app/src/main/java/com/example/myapplicationmd/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.myapplicationmd;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity  {
 
@@ -10,7 +14,8 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         //Mostrar el fragment
         if (savedInstanceState == null) {
             getSupportFragmentManager()
@@ -18,5 +23,22 @@ public class MainActivity extends AppCompatActivity  {
                     .add(R.id.container, new LoginfragmentActivity())
                     .commit();
         }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.jair:
+                return true;
+            case R.id.ariana:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
